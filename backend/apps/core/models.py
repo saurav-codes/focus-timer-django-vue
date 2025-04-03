aa28000ae5be5d0b16712a6ff1352b4268df62b6
saurav-codes <selftaughtdev@pm.me>
2025-04-03 15:47:59 +0530

    added more fields to Task model

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from timezone_field import TimeZoneField
 
-# Create your models here.
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     order = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
     is_in_brain_dump = models.BooleanField(default=True)
-    tags = TaggableManager(blank=True)
     is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    start_at = models.DateTimeField(blank=True, null=True)
+    end_at = models.DateTimeField(blank=True, null=True)
+    timezone = TimeZoneField(blank=True, null=True)
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
