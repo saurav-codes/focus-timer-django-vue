80798db9fe7ff4f834a4a1ff8da95cf0a924374e
saurav-codes <selftaughtdev@pm.me>
2025-04-06 07:17:52 +0530

    removed timezones handling for now

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from taggit.managers import TaggableManager
-from timezone_field import TimeZoneField
+# from timezone_field import TimeZoneField
 
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    order = models.PositiveIntegerField(default=0)
+    order = models.PositiveSmallIntegerField(default=0)
     is_in_brain_dump = models.BooleanField(default=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,11 +16,11 @@ class Task(models.Model):
     # so we can fetch this info from user's browser
     # we may need to handle it differently if user is let's say travelling
     # because he will add the task in diff timezones.
-    timezone = TimeZoneField(blank=True, null=True)
+    # timezone = TimeZoneField(blank=True, null=True)
     tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['order']
--- backend/apps/core/serializers.py
+++ backend/apps/core/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    # timezone = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
         fields = '__all__'
+
+    # def get_timezone(self, obj):
+    #     if obj.timezone:
+    #         return obj.timezone.key
+    #     return None
+
+    # def save(self, **kwargs):
+    #     start_at = self.validated_data.get('start_at')
+    #     if start_at:
+    #         self.validated_data["timezone"] = start_at.tzname()
+    #     return super().save(**kwargs)
