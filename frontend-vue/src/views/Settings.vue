adedb2af64a72e1ce01b14a566863e8e5387c8c5
saurav-codes <selftaughtdev@pm.me>
2025-04-19 08:10:12 +0530

    Fetch user data on Settings view mount and display it

--- frontend-vue/src/views/Settings.vue
+++ frontend-vue/src/views/Settings.vue
@@ -1,10 +1,22 @@
 <script setup>
+import { useAuthStore } from '../stores/authStore';
+import { onMounted, ref } from 'vue';
+
+const authStore = useAuthStore();
+const userData = ref({});
+
+onMounted(() => {
+  authStore.fetchUserData().then((data) => {
+    userData.value = data;
+  });
+});
 </script>
 
 <template>
   <div>
     <main>
       <h2>settings will be here</h2>
+      <code>{{ userData }}</code>
     </main>
   </div>
 </template>
