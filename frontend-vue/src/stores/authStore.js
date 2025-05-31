48477d1f1c996fde7e1724022bc00898274c5382
saurav-codes <selftaughtdev@pm.me>
2025-05-31 14:20:49 +0530

    minor fix

--- frontend-vue/src/stores/authStore.js
+++ frontend-vue/src/stores/authStore.js
@@ -5,7 +5,7 @@ import { useLocalStorage } from '@vueuse/core'
 
 const userDataLocalStorage = useLocalStorage('userData', {})
 const isAuthenticatedLocalStorage = useLocalStorage('isAuthenticated', false)
-const BACKEND_BASE_URL = 'http://localhost:8000/'
+// const BACKEND_BASE_URL = 'http://localhost:8000/'
 const BACKEND_BASE_URL = 'https://tymr.online/'
 
 // setting this to allow cookies to be set by
