b214cf200d256956e833464d009b3b3f3232d35c
saurav-codes <selftaughtdev@pm.me>
2025-06-04 06:13:34 +0530

    Configure Logfire for Celery worker and beat

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,6 +1,7 @@
 import os
-
+import logfire
 from celery import Celery
+from celery.signals import worker_init, beat_init
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
@@ -15,3 +16,15 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+@worker_init.connect()
+def init_worker(*args, **kwargs):
+    logfire.configure(service_name="worker")
+    logfire.instrument_celery()
+
+
+@beat_init.connect()
+def init_beat(*args, **kwargs):
+    logfire.configure(service_name="beat")
+    logfire.instrument_celery()
