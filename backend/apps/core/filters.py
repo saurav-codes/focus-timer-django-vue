21f6b4eef23fca0e3bb508bc32d9f0df833c8a23
saurav-codes <selftaughtdev@pm.me>
2025-06-06 17:25:19 +0530

    Refactor date filters in TaskFilter to use custom filter methods for improved clarity and functionality

--- backend/apps/core/filters.py
+++ backend/apps/core/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 from .models import Task, Project
 from taggit.models import Tag
+from django.db.models import Q
 
 
 class TaskFilter(filters.FilterSet):
@@ -8,9 +9,19 @@ class TaskFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name="tags__name", to_field_name="name", queryset=Tag.objects.all()
     )
-    start_date = filters.DateFilter(field_name="column_date", lookup_expr="date__gte")
-    end_date = filters.DateFilter(field_name="column_date", lookup_expr="date__lte")
+    start_date = filters.DateFilter(method="filter_start_date")
+    end_date = filters.DateFilter(method="filter_end_date")
 
     class Meta:
         model = Task
         fields = ["project", "tags", "start_date", "end_date"]
+
+    def filter_start_date(self, queryset, name, value):
+        return queryset.filter(
+            Q(column_date__date__gte=value) | Q(column_date__isnull=True)
+        )
+
+    def filter_end_date(self, queryset, name, value):
+        return queryset.filter(
+            Q(column_date__date__lte=value) | Q(column_date__isnull=True)
+        )
