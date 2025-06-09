b656eeacde367b6337552f200f5c7fb766af5a09
saurav-codes <selftaughtdev@pm.me>
2025-06-09 22:01:53 +0530

    feat: add PWA support with Vite PWA plugin and necessary assets
    - Updated package.json to include PWA-related dependencies: @vite-pwa/assets-generator, @vue/cli-plugin-pwa, and vite-plugin-pwa.
    - Added PWA icon (pwa-icon.svg) and various apple splash screen images for different device sizes.
    - Configured Vite to use the VitePWA plugin with a manifest for the application, including icons and theme color.

--- frontend-vue/vite.config.js
+++ frontend-vue/vite.config.js
@@ -3,10 +3,54 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        './public/favicon.ico',
+        './src/assets/meta/apple-icon-180.png',
+        './src/assets/meta/manifest-icon-512.maskable.png',
+      ],
+      manifest: {
+        name: 'Tymr Online',
+        short_name: 'Tymr',
+        description: 'Lets Plan your day with Tymr',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './src/assets/meta/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: './src/assets/meta/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: './src/assets/meta/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: './src/assets/meta/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
